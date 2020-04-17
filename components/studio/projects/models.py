74ea880253de38a05fc4c62085e039328a5f2847
Morgan <morgan@scaleoutsystems.com>
2020-04-17 22:14:11 +0200

    FEATURE/adding_studio

---
+++ components/studio/projects/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+import string
+import random
+
+DEFAULT_ENVIRONMENT_ID = 1
+
+
+class Environment(models.Model):
+    name = models.CharField(max_length=512)
+    slug = models.CharField(max_length=512, blank=True, null=True)
+    dockerfile = models.TextField(default='FROM jupyter/base-notebook')
+    startup = models.TextField(null=True, blank=True)
+    teardown = models.TextField(null=True, blank=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class ProjectManager(models.Manager):
+    def generate_passkey(self, length=20):
+        import secrets
+        import string
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        return password
+
+    def create_project(self, name, owner, description, repository):
+        letters = string.ascii_lowercase
+        slug_extension = ''.join(random.choice(letters) for i in range(3))
+
+        slug = '{}-{}'.format(slugify(name), slug_extension)
+        key = self.generate_passkey()
+        secret = self.generate_passkey(40)
+        environment = Environment(name=slug, slug=slug, dockerfile='FROM jupyter/minimal-notebook', startup='',
+                                  teardown='')
+        environment.save()
+        project = self.create(name=name, owner=owner, slug=slug, project_key=key, project_secret=secret,
+                              description=description, environment=environment, repository=repository,
+                              repository_imported=False)
+
+        return project
+
+
+class Project(models.Model):
+    objects = ProjectManager()
+
+    name = models.CharField(max_length=512)
+    description = models.TextField(null=True, blank=True)
+    slug = models.CharField(max_length=512, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='owner')
+    authorized = models.ManyToManyField(User, blank=True)
+    image = models.CharField(max_length=2048, blank=True, null=True)
+
+    project_key = models.CharField(max_length=512)
+    project_secret = models.CharField(max_length=512)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    repository = models.CharField(max_length=512, null=True, blank=True)
+    repository_imported = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Name: {} Description: {}".format(self.name, self.description)
+
+    environment = models.ForeignKey('projects.Environment', on_delete=models.DO_NOTHING, default=DEFAULT_ENVIRONMENT_ID)
---
+++ components/studio/projects/views.py
@@ -0,0 +1,136 @@
+from django.shortcuts import render, reverse
+from .models import Project
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from .exceptions import ProjectCreationException
+from .helpers import create_project_resources, delete_project_resources
+from django.contrib.auth.models import User
+from django.conf import settings as sett
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@login_required(login_url='/accounts/login')
+def index(request):
+    template = 'index_projects.html'
+    projects = Project.objects.filter(owner=request.user)
+    return render(request, template, locals())
+
+
+@login_required(login_url='/accounts/login')
+def settings(request, user, project_slug):
+    template = 'settings.html'
+    project = Project.objects.filter(owner=request.user).filter(slug=project_slug).first()
+    url_domain = sett.DOMAIN
+    return render(request, template, locals())
+
+
+@login_required(login_url='/accounts/login')
+def change_environment(request, user, project_slug):
+    project = Project.objects.filter(slug=project_slug).first()
+    environment = project.environment
+
+    if request.method == 'POST':
+        dockerfile = request.POST.get('dockerfile', '')
+        teardown = request.POST.get('teardown', '')
+        setup = request.POST.get('setup', '')
+
+        if dockerfile is not '':
+            environment.dockerfile = dockerfile
+        if teardown is not '':
+            environment.teardown = teardown
+        if setup is not '':
+            environment.setup = setup
+        environment.save()
+        from .helpers import create_environment_image
+        create_environment_image(project)
+    return HttpResponseRedirect(
+        reverse('projects:settings', kwargs={'user': request.user, 'project_slug': project.slug}))
+
+
+@login_required(login_url='/accounts/login')
+def create(request):
+    template = 'index_projects.html'
+
+    if request.method == 'POST':
+        name = request.POST.get('name', 'default')
+        access = request.POST.get('access', 'org')
+        description = request.POST.get('description', '')
+        repository = request.POST.get('repository', '')
+        project = Project.objects.create_project(name=name, owner=request.user, description=description,
+                                                 repository=repository)
+
+        success = True
+        try:
+            create_project_resources(project, repository=repository)
+        except ProjectCreationException as e:
+            print("ERROR: could not create project resources")
+            success = False
+
+        if success:
+            project.save()
+
+        next_page = request.POST.get('next', '/projects/{}/{}'.format(request.user, project.slug))
+
+        return HttpResponseRedirect(next_page, {'message': 'Created project'})
+
+    return render(request, template, locals())
+
+
+@login_required(login_url='/accounts/login')
+def details(request, user, project_slug):
+    template = 'project.html'
+
+    project = None
+    message = None
+
+    try:
+        owner = User.objects.filter(username=user).first()
+        project = Project.objects.filter(owner=owner, slug=project_slug).first()
+    except Exception as e:
+        message = 'No project found'
+
+    filename = None
+    readme = None
+    import requests as r
+    url = 'http://{}-file-controller/readme'.format(project.slug)
+    try:
+        response = r.get(url)
+        if response.status_code == 200 or response.status_code == 203:
+            payload = response.json()
+            if payload['status'] == 'OK':
+                filename = payload['filename']
+                readme = payload['readme']
+    except Exception as e:
+        logger.error("Failed to get response from {} with error: {}".format(url, e))
+
+    return render(request, template, locals())
+
+
+@login_required(login_url='/accounts/login')
+def delete(request, user, project_slug):
+    next_page = request.GET.get('next', '/projects/')
+
+    owner = User.objects.filter(username=user).first()
+    project = Project.objects.filter(owner=owner, slug=project_slug).first()
+
+    retval = delete_project_resources(project)
+
+    if not retval:
+        next_page = request.GET.get('next', '/projects/{}/{}'.format(request.user, project.slug))
+        print("could not delete!")
+        return HttpResponseRedirect(next_page, {'message': 'Error during project deletion'})
+
+    print("PROJECT RESOURCES DELETED SUCCESFULLY!")
+
+    project.delete()
+
+    return HttpResponseRedirect(next_page, {'message': 'Deleted project successfully.'})
+
+
+def auth(request):
+    if request.user.is_authenticated:
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=403)
