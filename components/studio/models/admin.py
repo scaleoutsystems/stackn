6956a85dd2e6ef9219075b2f67614efb8a8ea7aa
Morgan <morgan@scaleoutsystems.com>
2020-04-17 22:14:11 +0200

    FEATURE/adding_studio

---
+++ components/studio/models/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Model
+
+admin.site.register(Model)
---
+++ components/studio/models/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ModelsConfig(AppConfig):
+    name = 'models'
+    verbose_name = 'Scaleout Models'
