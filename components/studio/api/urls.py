406d93eace0cc23b396b0d4cfb66dc9acddba15a
Desislava Stoyanova <dstoyanova929@gmail.com>
2020-04-20 11:42:54 +0200

    API response filtering against the current user

--- components/studio/api/urls.py
+++ components/studio/api/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from .views import ModelViewSet, ReportViewSet, ReportGeneratorViewSet, ProjectViewSet, GetProjectInfo
+from .views import ModelList, ReportList, ReportGeneratorList, ProjectList
 from rest_framework.authtoken.views import obtain_auth_token
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
-router.register(r'models', ModelViewSet)
-router.register(r'reports', ReportViewSet)
-router.register(r'generators', ReportGeneratorViewSet)
-router.register(r'projects', ProjectViewSet)
+router.register(r'models', ModelList, basename='model')
+router.register(r'reports', ReportList, basename='report')
+router.register(r'generators', ReportGeneratorList, basename='report_generator')
+router.register(r'projects', ProjectList, basename='project')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('user/<str:project_owner_name>/project/<str:project_name>', GetProjectInfo.as_view()),
     path('api-token-auth', obtain_auth_token, name='api_token_auth'),
 ]
