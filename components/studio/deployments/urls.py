96bdc675040c927bbc38eb39dc082664e6edb73a
Morgan <morgan@scaleoutsystems.com>
2020-05-04 14:17:31 +0200

    FEATURE: Implemented #39 to change routing and default template.

--- components/studio/deployments/urls.py
+++ components/studio/deployments/urls.py
@@ -4,16 +4,13 @@ from . import views
 app_name = 'deployments'
 
 urlpatterns = [
-    path('deploy/<int:id>', views.deploy, name='deploy'),
-    path('undeploy/<int:id>', views.undeploy, name='undeploy'),
     path('', views.index, name='index'),
-    path('definition', views.deployment_definition_index, name='deployment_definition_index'),
-    path('definition/add', views.deployment_definition_add, name='deployment_definition_add'),
-    path('definition/edit/<id>', views.deployment_definition_edit, name='deployment_definition_edit'),
-
-    path('<user>/<project>', views.deployment_index, name='deployment_index'),
-    path('<user>/<project>/add', views.deployment_add, name='deployment_add'),
-    path('<user>/<project>/edit/<id>', views.deployment_edit, name='deployment_edit'),
-
-
+    path('deployments/deploy/<int:id>', views.deploy, name='deploy'),
+    path('deployments/undeploy/<int:id>', views.undeploy, name='undeploy'),
+    path('deployments/definition', views.deployment_definition_index, name='deployment_definition_index'),
+    path('deployments/definition/add', views.deployment_definition_add, name='deployment_definition_add'),
+    path('deployments/definition/edit/<id>', views.deployment_definition_edit, name='deployment_definition_edit'),
+    path('deployments/<user>/<project>', views.deployment_index, name='deployment_index'),
+    path('deployments/<user>/<project>/add', views.deployment_add, name='deployment_add'),
+    path('deployments/<user>/<project>/edit/<id>', views.deployment_edit, name='deployment_edit'),
 ]
