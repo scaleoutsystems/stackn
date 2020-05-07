7f77bfe6b128748b4ebd35fb9e5ba198b18dfa1f
Desislava Stoyanova <dstoyanova929@gmail.com>
2020-05-07 17:23:20 +0200

    Model Cards and Model Details views

--- components/studio/models/views.py
+++ components/studio/models/views.py
@@ -63,11 +63,54 @@ def create(request, user, project):
 @login_required(login_url='/accounts/login')
 def details(request, user, project, id):
     project = Project.objects.filter(slug=project).first()
-    template = 'models_details.html'
     model = Model.objects.filter(id=id).first()
     deployments = DeploymentInstance.objects.filter(model=model)
 
-    return render(request, template, locals())
+    report_generators = ReportGenerator.objects.filter(project=project)
+
+    unfinished_reports = Report.objects.filter(status='P').order_by('created_at')
+    for report in unfinished_reports:
+        populate_report_by_id(report.id)
+
+    reports = Report.objects.filter(model__id=id, status='C').order_by('-created_at')
+
+    report_dtos = []
+    for report in reports:
+        report_dtos.append({
+            'id': report.id,
+            'description': report.description,
+            'created_at': report.created_at,
+            'filename': get_download_link(project.pk, 'report_{}.json'.format(report.id))
+        })
+
+    if request.method == 'POST':
+        file_path = None
+        form = GenerateReportForm(request.POST)
+        if form.is_valid():
+            generator_id = int(form.cleaned_data['generator_file'])
+            generator_object = ReportGenerator.objects.filter(pk=generator_id).first()
+
+            file_path = 'reports/{}'.format(generator_object.generator)
+
+            instance = {
+                'id': str(uuid.uuid4()),
+                'path_to_file': file_path,
+                'model_uid': model.uid,
+                'project_name': project.slug
+            }
+
+            new_report = Report(model=model, report="", job_id=instance['id'], generator=generator_object, status='P')
+            new_report.save()
+
+            from reports.jobs import run_job
+
+            run_job(instance)
+
+            return HttpResponseRedirect('/projects/{}/{}/models/'.format(user, project.slug))
+    else:
+        form = GenerateReportForm()
+
+    return render(request, 'models_details.html', locals())
 
 
 @login_required(login_url='/accounts/login')
