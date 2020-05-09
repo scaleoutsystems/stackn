44adcf65ee93580082ea41be0968b8fef7e84522
Stefan Hellander <stefan@scaleoutsystems.com>
2020-05-09 22:57:00 +0200

    Ensures telepresence gets the right settings module

--- components/studio/scripts/run_web.sh
+++ components/studio/scripts/run_web.sh
@@ -21,5 +21,8 @@ echo "loading seed data..."
 python3 manage.py loaddata projects/fixtures/fixtures.json
 python3 manage.py loaddata projects/fixtures/data.json
 python3 manage.py loaddata deployments/fixtures/data.json
+[[ -z "${TELEPRESENCE_ROOT}" ]];  echo "Copy settings from Telepresence root directory" && \
+    cp $TELEPRESENCE_ROOT/app/studio/settings.py studio/tele_settings.py && \
+    export DJANGO_SETTINGS_MODULE=studio.tele_settings
 echo "starting serving..."
 python3 manage.py runserver 0.0.0.0:8080
