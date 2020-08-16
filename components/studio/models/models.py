f2831b2a9180c3db683cae7c0792d51eeb3b8567
Stefan Hellander <stefan@scaleoutsystems.com>
2020-08-16 19:35:13 +0200

    STACKN-4: Models now have version instead of tag, default versioning system is also configurable by the user.

--- components/studio/models/models.py
+++ components/studio/models/models.py
@@ -1,10 +1,51 @@
+from django.conf import settings
 from django.db import models
 from django.dispatch import receiver
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
+from django import forms
+from django.utils.module_loading import import_string
 from deployments.models import DeploymentInstance
+from ast import literal_eval
+from functools import cmp_to_key
+
+VERSION_CLASS = import_string(settings.VERSION_BACKEND)
+
+def compare_version(v1, v2):
+    v1obj = VERSION_CLASS(v1.version)
+    v2obj = VERSION_CLASS(v2.version)
+    if v1obj < v2obj:
+        return -1
+    elif v1obj > v2obj:
+        return 1
+    else:
+        return 0
+
+class ModelManager(models.Manager):
+
+    def sorted_by_version(self, model_name, project):
+        models = super().get_queryset().filter(project=project, name=model_name)
+        if models:
+            models = sorted(models, key=cmp_to_key(compare_version))
+            return models
+        return []
+
+    # Get latest version.
+    def latest(self, model_name, project):
+        # Get all models in the project
+        models = super().get_queryset().filter(project=project, name=model_name)
+        if models:
+            # Sort by version
+            models = sorted(models, key=cmp_to_key(compare_version))
+            for model in models:
+                print('{}-{}'.format(model.name, model.version))
+            return models[0]
+
+        return []
 
 class Model(models.Model):
 
+    objects_version = ModelManager()
+    objects = models.Manager()
     PRIVATE = 'PR'
     LIMITED = 'LI'
     PUBLIC = 'PU'
@@ -24,6 +65,8 @@ class Model(models.Model):
     ]
     uid = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
+    version = models.CharField(max_length=255)
+    release_type = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True, blank=True)
     access = models.CharField(max_length=2, choices=ACCESS, default=PRIVATE)
     resource = models.URLField(max_length=2048, null=True, blank=True)
@@ -35,12 +78,35 @@ class Model(models.Model):
         related_name='model_owner',
         null=True)
     status = models.CharField(max_length=2, choices=STATUS, default=CREATED)
-    tag = models.CharField(max_length=10, default='latest')
+    # tag = models.CharField(max_length=10, default='latest')
     
     class Meta:
-        unique_together = ('name', 'tag')
+        unique_together = ('name', 'version', 'project')
+
     def __str__(self): 
         return "{name}".format(name=self.name)
+
+@receiver(pre_save, sender=Model, dispatch_uid='model_pre_save_signal')
+def pre_save_model(sender, instance, using, **kwargs):
+    # Load version backend
+    
+    # Set version
+    release_type = instance.release_type
+    # If version is not already set, create new release
+    if not instance.version:
+        # Get latest release and bump:
+        model = Model.objects_version.latest(instance.name, instance.project)
+        if not model:
+            # This is the first release
+            new_version = VERSION_CLASS()
+        else:
+            new_version = VERSION_CLASS(model.version)
+        
+
+        release_status, instance.version = new_version.release(release_type)
+        print('New version: '+instance.version)
+        if not release_status:
+            raise Exception('Failed to create new release for model {}-{}, release type {}.'.format(instance.name, instance.version, release_type))
 
 @receiver(pre_delete, sender=Model, dispatch_uid='model_pre_delete_signal')
 def pre_delete_deployment(sender, instance, using, **kwargs):
