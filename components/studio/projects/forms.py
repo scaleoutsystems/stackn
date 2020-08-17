f27bd5eaf4c933ef93424f463f4537f0d281de8b
Desislava Stoyanova <dstoyanova929@gmail.com>
2020-08-17 13:22:14 +0200

    Grant access to a Project without using the Django Admin.
    With the integration of Keycloak, users don't have access to the Django Admin page which requires an addition to the project's settings page for granting access to other users.
    
    More info -> [JIRA](https://scaleoutsystems.atlassian.net/browse/STACKN-110)

--- components/studio/projects/forms.py
+++ components/studio/projects/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class TransferProjectOwnershipForm(forms.Form):
@@ -8,3 +9,13 @@ class TransferProjectOwnershipForm(forms.Form):
 class PublishProjectToGitHub(forms.Form):
     user_name = forms.CharField(label='GitHub username', max_length=256)
     user_password = forms.CharField(label='GitHub password', widget=forms.PasswordInput(), max_length=256)
+
+
+class GrantAccessForm(forms.Form):
+    platform_users = User.objects.all()
+
+    OPTIONS = []
+    for user in platform_users:
+        OPTIONS.append((user.pk, user.username))
+
+    selected_users = forms.MultipleChoiceField(widget=forms.SelectMultiple, choices=OPTIONS)
