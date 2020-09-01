e53a479cf2514a7228dd99b81548bdb7ca9d6b45
Stefan Hellander <stefan@scaleoutsystems.com>
2020-09-01 12:59:38 +0200

    refactor labs creation

--- components/studio/monitor/views.py
+++ components/studio/monitor/views.py
@@ -11,16 +11,42 @@ from projects.models import Project
 from .helpers import get_total_labs_cpu_usage_60s, get_total_labs_memory_usage_60s
 from .helpers import get_labs_cpu_requests, get_labs_memory_requests
 from .helpers import get_total_cpu_usage_60s_ts
+from .helpers import get_resource, get_all
+from labs.models import Session
+
 
 @login_required(login_url='/accounts/login')
 def overview(request, user, project):
     template = 'monitor_overview.html'
     project = Project.objects.filter(slug=project).first()
 
-    labs_cpu_requests = get_labs_cpu_requests(project.slug)
-    labs_memory_requests = get_labs_memory_requests(project.slug)
-    labs_cpu_usage = get_total_labs_cpu_usage_60s(project.slug)
-    labs_memory_usage = get_total_labs_memory_usage_60s(project.slug)
+    resource_types = ['lab', 'deployment']
+    q_types = ['requests', 'limits']
+    r_types = ['memory_bytes', 'cpu_cores']
+
+    resource_status = dict()
+    for resource_type in resource_types:
+        resource_status[resource_type] = dict()
+        for q_type in q_types:
+            resource_status[resource_type][q_type] = dict()
+            for r_type in r_types:
+                tmp = get_resource(project.slug, resource_type, q_type, r_type)
+                if r_type == 'memory_bytes':
+                    tmp ="{:.2f}".format(float(tmp)/1e9*0.931323)
+                else:
+                    tmp = "{:.2f}".format(float(tmp))
+                resource_status[resource_type][q_type][r_type] = tmp
+
+    total_cpu = float(resource_status['lab']['limits']['cpu_cores'])+float(resource_status['deployment']['limits']['cpu_cores'])
+    total_mem = float(resource_status['lab']['limits']['memory_bytes'])+float(resource_status['deployment']['limits']['memory_bytes'])
+
+    labs = Session.objects.filter(project=project)
+    for lab in labs:
+        print(lab.lab_session_owner)
+
+    # print(total_cpu)
+    # print(total_mem)
+    # print(get_all())
 
     return render(request, template, locals())
 
