3de25f76a7268e3dfa433689af19ef6e8b66b0e4
Adam Hagevall <adamhagevall@Adams-MBP.lan>
2020-11-24 17:03:50 +0100

    New model Metadata that stores tracked training data in Django database. More detailed data version control. Now possible to run stackn train without storing data in Studio and without passing a model name or code version.

--- components/studio/api/urls.py
+++ components/studio/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include
 from django.urls import path
 import rest_framework.routers as drfrouters
-from .views import ModelList, ModelLogList, ReportList, ReportGeneratorList, ProjectList, DeploymentInstanceList, \
+from .views import ModelList, ModelLogList, MetadataList, ReportList, ReportGeneratorList, ProjectList, DeploymentInstanceList, \
     DeploymentDefinitionList, LabsList, MembersList, DatasetList
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_nested import routers
@@ -22,6 +22,7 @@ models_router.register(r'labs', LabsList, base_name='lab')
 models_router.register(r'members', MembersList, base_name='members')
 models_router.register(r'dataset', DatasetList, base_name='dataset')
 models_router.register(r'modellogs', ModelLogList, base_name='modellog')
+models_router.register(r'metadata', MetadataList, base_name='metadata')
 
 router.register(r'deploymentInstances', DeploymentInstanceList, base_name='deploymentInstance')
 router.register(r'deploymentDefinitions', DeploymentDefinitionList, base_name='deploymentDefinition')
