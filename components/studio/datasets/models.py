1edb2d4b1ac8ddba6bb4b0a1461afb79ba59bdf4
Carl Wikström <c.wikstrom@outlook.com>
2020-11-25 09:57:06 +0100

    django form for datasheet

--- components/studio/datasets/models.py
+++ components/studio/datasets/models.py
@@ -46,13 +46,9 @@ class Dataset(models.Model):
     files = models.ManyToManyField(FileModel, blank=True)
     created_by = models.CharField(max_length=255) # Username
     created_on = models.DateTimeField(auto_now_add=True)
+    datsheet = models.FileField(upload_to='datasheets/', default=None)
     class Meta:
         unique_together = ('name', 'version', 'project_slug')
-
-class Datasheet(models.Model):
-    objects_version = ModelManager()
-    objects = models.Manager
-    name = models.CharField(max_length=255)
 
 @receiver(pre_save, sender=Dataset, dispatch_uid='dataset_pre_save_signal')
 def pre_save_model(sender, instance, using, **kwargs):
