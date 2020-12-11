3b8f1eafb7f83734d083185b1f7cc4a4a35b2548
Desislava Stoyanova <dstoyanova929@gmail.com>
2020-12-11 08:04:45 +0100

    Resolved code review comments.

--- cli/scaleout/cli/get_cmd.py
+++ cli/scaleout/cli/get_cmd.py
@@ -23,15 +23,10 @@ def get_settings_cmd(ctx):
     """
     List STACKn settings needed to set up the CLI client.
     """
-
     studio_host = input("Studio host: ")
     url = "{}/api/settings".format(studio_host)
-    r = requests.get(url)
-    if (r.status_code < 200 or r.status_code > 299):
-        print("Couldn't get studio settings.")
-        print("Returned status code: {}".format(r.status_code))
-        print("Reason: {}".format(r.reason))
-    else:
+    try:
+        r = requests.get(url)
         studio_settings = json.loads(r.content)["data"]
 
         names = ['Setting', 'Value']
@@ -42,6 +37,11 @@ def get_settings_cmd(ctx):
             row = [item[k] for k in keys]
             x.add_row(row)
         print(x)
+    except Exception as e:
+        print("Couldn't get studio settings.")
+        print("Returned status code: {}".format(r.status_code))
+        print("Reason: {}".format(r.reason))
+        print("Error: {}".format(e))
 
 @get_cmd.command('models')
 @click.pass_context
