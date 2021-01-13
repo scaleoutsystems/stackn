7c58dc5cc6dc8e579b1e76e2a51a8bad1b1df706
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-13 13:32:39 +0100

    Changed from index.html to avoid confusion

--- components/studio/studio_admin/views.py
+++ components/studio/studio_admin/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'studio_admin/index.html', locals())
+    return render(request, 'studio_admin_index.html', locals())
