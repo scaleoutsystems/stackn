9977a011b397113ee1b69c4f04c3ac8b81c8447e
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-13 17:26:46 +0100

    Update urls.py

--- components/studio/studio_admin/urls.py
+++ components/studio/studio_admin/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'studio_admin'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
     path('activity', views.load_recent_activity, name='recent_activity')
 ]
