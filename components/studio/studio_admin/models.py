57686c031043d5972bd8be816a5f736331795af5
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-14 14:26:20 +0100

    Update models.py

--- components/studio/studio_admin/models.py
+++ components/studio/studio_admin/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class ActivityLog(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=null)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     headline = models.CharField(max_length=256)
     description = models.CharField(max_length=512)
     created_at = models.DateTimeField(auto_now_add=True)
