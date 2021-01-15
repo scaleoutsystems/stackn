3afdf37738f99a5a1bc46ba265a17c2bb2c3b465
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-15 10:33:01 +0100

    Added module template

--- components/studio/studio_admin/urls.py
+++ components/studio/studio_admin/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'studio_admin'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<str:module>', views.load_module_objects, name='load_module_objects')
 ]
--- components/studio/studio_admin/views.py
+++ components/studio/studio_admin/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from datetime import datetime, timedelta
 from .models import ActivityLog
+from projects.models import Project
 
 def index(request):
     activity_logs = ActivityLog.objects.filter(user=request.user).order_by('-created_at')[:5]
 
     return render(request, 'studio_admin_index.html', locals())
+
+def load_module_objects(request, module):
+    objects = []
+
+    if module == 'projects':
+        objects = load_projects()
+
+    return render(request, 'studio_admin_module.html', {'objects': objects, 'module': module})
+
+def load_projects():
+    projects = Project.objects.all()
+    return projects
