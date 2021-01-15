fd16f371d7fe207a87e018b24b0585d11113d1c3
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-15 09:42:45 +0100

    Added an icon button to the side navigation

--- components/studio/studio_admin/views.py
+++ components/studio/studio_admin/views.py
@@ -3,6 +3,6 @@ from datetime import datetime, timedelta
 from .models import ActivityLog
 
 def index(request):
-    activity_logs = ActivityLog.objects.filter(user=request.user).order_by('-created_at')[:10]
+    activity_logs = ActivityLog.objects.filter(user=request.user).order_by('-created_at')[:5]
 
     return render(request, 'studio_admin_index.html', locals())
