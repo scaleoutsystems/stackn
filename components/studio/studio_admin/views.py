2de50aebc0bb963d436ee9954f67c6d7c8ea79e2
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-18 09:45:22 +0100

    Added HTML templates for the different model classes.

--- components/studio/studio_admin/views.py
+++ components/studio/studio_admin/views.py
@@ -2,21 +2,51 @@ from django.shortcuts import render
 from datetime import datetime, timedelta
 from .models import ActivityLog
 from projects.models import Project
+from models.models import Model
+from labs.models import Session
+from deployments.models import DeploymentInstance
 
 def index(request):
-    module = 'projects'
+    str_projects = 'projects'
+    str_models = 'models'
+    str_labs = 'labs'
+    str_deployments = 'deployments'
+
     activity_logs = ActivityLog.objects.filter(user=request.user).order_by('-created_at')[:5]
 
     return render(request, 'studio_admin_index.html', locals())
 
 def load_module_objects(request, module):
-    objects = []
+    objects = None
+    template = None
 
     if module == 'projects':
         objects = load_projects()
+        template = "studio_admin_projects.html"
+    if module == 'models':
+        objects = load_models()
+        template = "studio_admin_models.html"
+    if module == 'labs':
+        objects = load_labs()
+        template = "studio_admin_labs.html"
+    if module == 'deployments':
+        objects = load_deployments()
+        template = "studio_admin_deployments.html"
 
-    return render(request, 'studio_admin_module.html', {'objects': objects, 'module': module})
+    return render(request, template, {'objects': objects})
 
 def load_projects():
     projects = Project.objects.all()
     return projects
+
+def load_models():
+    models = Model.objects.all()
+    return models
+
+def load_labs():
+    labs = Session.objects.all()
+    return labs
+
+def load_deployments():
+    deployments = DeploymentInstance.objects.all()
+    return deployments
