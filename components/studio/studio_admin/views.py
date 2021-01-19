8591b95b8655d0411a323a478dea1b68342f81d1
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-19 15:29:45 +0100

    Reverted changes for filter

--- components/studio/studio_admin/views.py
+++ components/studio/studio_admin/views.py
@@ -56,16 +56,19 @@ def load_project_resources(request):
     return render(request, template, locals())
 
 
-def get_resource_value(projects_resources, project_slug, args):
+@register.filter
+def get_resource_value(projects_resources, args):
     arg_list = [arg.strip() for arg in args.split(',')]
 
-    if len(arg_list) == 1:
-        total_x = arg_list[0]
+    project_slug = arg_list[0]
+
+    if len(arg_list) == 2:
+        total_x = arg_list[1]
         return projects_resources[project_slug][total_x]
 
-    resource_type = arg_list[0]
-    q_type = arg_list[1]
-    r_type = arg_list[2]
+    resource_type = arg_list[1]
+    q_type = arg_list[2]
+    r_type = arg_list[3]
 
     return projects_resources[project_slug][resource_type][q_type][r_type]
 
