ac0e6f3468f2265b95a9ecce7b751c09390cad88
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-01-19 16:28:31 +0100

    Update studio_admin_extras.py

--- components/studio/studio_admin/templatetags/studio_admin_extras.py
+++ components/studio/studio_admin/templatetags/studio_admin_extras.py
@@ -5,16 +5,17 @@ register = template.Library()
 
 @register.filter(name='get_resource_value')
 def get_resource_value(projects_resources, args):
+    print(args)
     arg_list = [arg.strip() for arg in args.split(',')]
+    print(arg_list)
+    # project_slug = arg_list[0]
 
-    project_slug = arg_list[0]
+    # if len(arg_list) == 2:
+    #     total_x = arg_list[1]
+    #     return projects_resources[project_slug][total_x]
 
-    if len(arg_list) == 2:
-        total_x = arg_list[1]
-        return projects_resources[project_slug][total_x]
+    # resource_type = arg_list[1]
+    # q_type = arg_list[2]
+    # r_type = arg_list[3]
 
-    resource_type = arg_list[1]
-    q_type = arg_list[2]
-    r_type = arg_list[3]
-
-    return projects_resources[project_slug][resource_type][q_type][r_type]
+    # return projects_resources[project_slug][resource_type][q_type][r_type]
