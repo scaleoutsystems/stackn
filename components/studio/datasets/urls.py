4318865d7876b89beef92d04c41ca113817e234c
Desislava Stoyanova <desislava@scaleoutsystems.com>
2021-02-23 18:04:57 +0100

    Refactored datasets module.

--- components/studio/datasets/urls.py
+++ components/studio/datasets/urls.py
@@ -5,6 +5,5 @@ app_name = 'datasets'
 
 urlpatterns = [
     path('<int:page_index>', views.page, name='page'),
-    path('<str:path_name>/<int:page_index>', views.path_page, name='path_page'),
     path('<int:page_index>/datasheet', views.datasheet, name='datasheet'),
 ]
