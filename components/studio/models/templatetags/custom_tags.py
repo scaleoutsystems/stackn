81271947b973ebd9e5cb9cf1629e34431e789166
Hamza Imran Saeed <hamza.saeed@scilifelab.uu.se>
2021-07-19 12:25:06 +0200

    add tags from private model view

--- components/studio/models/templatetags/custom_tags.py
+++ components/studio/models/templatetags/custom_tags.py
@@ -16,3 +16,20 @@ def exists(value, arg):
     if str(arg) in value:
         return True
     return False
+
+@register.filter(name='count_str')
+def split(value):
+    total_length = 0
+    num_tags = 0
+    for tag in value:
+        total_length += len(str(tag))
+        if total_length > 50:
+            break
+        num_tags += 1
+    if num_tags == 0:
+        num_tags = 1
+    return num_tags
+
+@register.filter(name='subtract')
+def subtract(value, arg):
+    return value - arg
