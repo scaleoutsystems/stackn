375fcb9867a9925cf6f47a42dd266d39321235d5
Morgan Ekmefjord <morgan@scaleoutsystems.com>
2021-10-31 15:58:12 +0100

    Remove modules not in use in default installation.

--- components/studio/datasets/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Dataset, FileModel
-
-admin.site.register(Dataset)
-admin.site.register(FileModel)
--- components/studio/studio_admin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudioAdminConfig(AppConfig):
-    name = 'studio_admin'
