f5540c6419ff0ddc1782ca968d7be9942a833f47
Morgan Ekmefjord <morgan@scaleoutsystems.com>
2021-10-31 21:44:52 +0100

    Fix fixtures, fix inclusion of going back to defacto settings file. Trim requirements.

--- components/studio/studio/celery.py
+++ components/studio/studio/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studio.settingslocal')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studio.settings')
 
 app = Celery('studio')
 
--- components/studio/studio/wsgi.py
+++ components/studio/studio/wsgi.py
@@ -13,6 +13,6 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 if not 'TELEPRESENCE_ROOT' in os.environ:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studio.settingslocal')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studio.settings')
 
 application = get_wsgi_application()
