c9a443c6a69254abd185b05f243302d1b58325ae
Morgan Ekmefjord <morgan@scaleoutsystems.com>
2021-11-01 09:01:46 +0100

    Test new build action.

--- components/studio/studio/views.py
+++ components/studio/studio/views.py
@@ -25,6 +25,7 @@ def register(request):
     context = {'form':form}
     return render(request,'registration/register.html',context)
 
+
 # Since this is a production feature, it will only work if DEBUG is set to False
 def handle_page_not_found(request, exception):
     return HttpResponseRedirect('/')
