98f50c801c8453198179c8c417925ffe6cfbd820
TechCarmat <techcarmat@pm.me>
2021-12-10 14:23:47 +0100

    Cleaning and updating portal app

--- components/studio/portal/views.py
+++ components/studio/portal/views.py
@@ -1,17 +1,6 @@
-import requests
-import uuid
-import time
-
-from django.shortcuts import render, HttpResponseRedirect, reverse, redirect
-from django.http import JsonResponse
+from django.shortcuts import render
 from django.conf import settings
-from django.utils.text import slugify
 from django.db.models import Q
-from django.template import engines
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-
-import modules.keycloak_lib as kc
 
 from apps.models import Apps, AppInstance
 from projects.models import Project
@@ -22,22 +11,18 @@ def index(request,id=0):
     except Exception as err:
         print("User not logged in.")
     base_template = 'base.html'
+
     if 'project' in request.session:
         project_slug = request.session['project']
-        is_authorized = kc.keycloak_verify_user_role(request, project_slug, ['member'])
-        if is_authorized:
-            try:
-                project = Project.objects.filter(Q(owner=request.user) | Q(authorized=request.user), status='active', slug=project_slug).first()
-                base_template = 'baseproject.html'
-            except TypeError as err:
-                project = []
-                print(err)
-            if not project:
-                base_template = 'base.html'
-    # if project_selected:
-    #     print("Project is selected")
-    #     print(project)
-    #     print(base_template)
+        try:
+            project = Project.objects.filter(Q(owner=request.user) | Q(authorized=request.user), status='active', slug=project_slug).first()
+            base_template = 'base.html'
+        except TypeError as err:
+            project = []
+            print(err)
+        if not project:
+            base_template = 'base.html'
+
     media_url = settings.MEDIA_URL
     
     # create session object to store info about app and their tag counts
@@ -91,6 +76,6 @@ def index(request,id=0):
         published_apps = tagged_published_apps
         
     request.session.modified = True
-
+    
     template = 'index_portal.html'
     return render(request, template, locals())
