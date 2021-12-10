77e80fdf87ea1a16ec2a9f578b983fe96cc6a56f
TechCarmat <techcarmat@pm.me>
2021-12-10 12:52:18 +0100

    Adding default_auto_field in all apps.py

--- components/studio/deployments/apps.py
+++ components/studio/deployments/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class DeploymentsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'deployments'
+    verbose_name = 'Studio Deployments'
