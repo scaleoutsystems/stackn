09e68a0429dd1d2f682013cff4291046a0dc156e
TechCarmat <techcarmat@pm.me>
2021-12-10 12:55:04 +0100

    Removing other keycloak related files

--- components/studio/studio/OIDCbackend.py
+++
@@ -1,7 +0,0 @@
-from mozilla_django_oidc.auth import OIDCAuthenticationBackend
-
-class OIDCbackend(OIDCAuthenticationBackend):
-    def get_username(self, claims):
-        print('local')
-        print(claims)
-        return claims.get('preferred_username')
