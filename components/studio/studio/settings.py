a6251b1264b5e7402f173de649aba7ef77fc7b9a
TechCarmat <techcarmat@pm.me>
2021-12-10 13:13:02 +0100

    Updating settings.py

--- components/studio/studio/settings.py
+++ components/studio/studio/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 AUTHENTICATION_BACKENDS = [
+    'social_core.backends.github.GithubOAuth2',
+    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
@@ -20,59 +23,68 @@ AUTHENTICATION_BACKENDS = [
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DOMAIN = 'platform.local'
+# Crispy Forms
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+DOMAIN = 'localhost'    # only for local development, otherwise x.x.x.x.nip.io
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'pyey3^@n)$id1tc3_g7xcb55n7ii1989jy#&%!yk^z(u1us4@*'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 else:
-    ALLOWED_HOSTS = ['platform.local']
+    ALLOWED_HOSTS = ['localhost']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Application definition
-
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+ ]
+
+THIRD_PARTY_APPS = [
+    # add apps which you install using pip
+    "crispy_forms",            
+    'corsheaders',
+    'django_celery_beat',
     'django_filters',
     'oauth2_provider',
-    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
-    'ingress',
-    'api',
-    'monitor',
-    'projects',
-    'models',
-    'reports',
-    'files',
-    'datasets',
-    'workflows',
-    'deployments',
-    'bootstrap_modal_forms',
-    'studio_admin',
-    'apps',
-    'django_plotly_dash',
-    'portal',
+    'social_django',
     'tagulous',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+LOCAL_APPS =[
+    # add local apps which you create using startapp
+    'api',
+    'apps',
+    'common',
+    'deployments',
+    'monitor',
+    'models',
+    'projects',
+    'portal',
+]
+
+# # Application definition
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# Related to oauth2_provider third-party apps
+OAUTH2_PROVIDER = {
+    # this is the list of available scopes
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope', 'groups': 'Access to your groups'}
 }
 
 MIDDLEWARE = [
@@ -84,15 +96,45 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',   # Add
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        #'rest_framework.permissions.IsAuthenticated',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    ],
+}
+
+# Main Url conf for loading all the routing path in Studio
+ROOT_URLCONF = 'studio.urls'
+
+# IMPORTANT: Must be encrypted as secrets in K8S
+# Github
+SOCIAL_AUTH_GITHUB_KEY = '<enter-github-application-key>'
+SOCIAL_AUTH_GITHUB_SECRET = ' <enter-github-application-key>'
+SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']   # Ask for the user's email
+
+# Google
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '<enter-google-application-key>'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '<enter-google-secret>'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['user:email']    # Ask for the user's email
+
+# Tagulous serialization settings
+SERIALIZATION_MODULES = {
+    'xml':    'tagulous.serializers.xml_serializer',
+    'json':   'tagulous.serializers.json',
+    'python': 'tagulous.serializers.python',
+    'yaml':   'tagulous.serializers.pyyaml',
+}
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # other finders..
+    # other finders
     'compressor.finders.CompressorFinder',
 )
-
-ROOT_URLCONF = 'studio.urls'
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -103,7 +145,7 @@ TEMPLATE_LOADERS = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(REPO_DIR, 'templates')],
+        'DIRS': [os.path.join(REPO_DIR, 'templates'), os.path.join(REPO_DIR, 'common/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,6 +153,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',  # Add
+                'social_django.context_processors.login_redirect', # Add
             ],
             'libraries': {
                 'custom_tags': 'models.templatetags.custom_tags',
@@ -124,24 +168,17 @@ ASGI_APPLICATION = 'studio.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'stack-studio-db',
-#         'PORT': 5432,
-#     }
-# }
-# Dummy backend here to allow for creating migrations locally.
+# Dummy backend here to allow for local development with docker-compose!
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -163,48 +200,32 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
-                    ]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(REPO_DIR, 'static/')
-
+# Media Files for Studio apps
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(REPO_DIR, 'media/')
 
+# Related to user registration and authetication workflow
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
 
-import socket
-
-# TODO remove after refactor
-API_HOSTNAME = 'localhost'
-API_PORT = 8080
-
-GIT_REPOS_ROOT = os.path.join(REPO_DIR, 'repos')
-GIT_REPOS_URL = '/repos/'
-
-REGISTRY_SVC = 'stack-docker-registry'
-CHART_CONTROLLER_URL = 'http://stack-chart-controller'
-STUDIO_URL = 'http://stack-studio:8080'
-
+# Specific to Studio stack:
+# Redis settings
 REDIS_PORT = 6379
 REDIS_DB = 0
 REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'platform-redis')
-
+# Celery settings
 CELERY_BROKER_URL = 'amqp://admin:LJqEG9RE4FdZbVWoJzZIOQEI@platform-rabbit:5672//'
 CELERY_RESULT_BACKEND = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)
 CELERY_TASK_SERIALIZER = 'json'
@@ -213,20 +234,11 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TIMEZONE = "UTC"
 CELERY_ENABLE_UTC = True
 
+# K8S deployment settings
+CHART_CONTROLLER_URL = 'http://stack-chart-controller'
 EXTERNAL_KUBECONF = True
+KUBECONFIG = "/root/.kube/config"
 NAMESPACE = 'default'
-STORAGECLASS = 'aws-efs'
-
-try:
-    from .settings_local import *
-except ImportError as e:
-    pass
-
-import os
-
-try:
-    apps = [os.environ.get("APPS").split(" ")]
-    for app in apps:
-        INSTALLED_APPS += [app]
-except Exception as e:
-    pass
+REGISTRY_SVC = 'stack-docker-registry'
+STORAGECLASS = 'microk8s-hostpath'
+STUDIO_URL = 'http://localhost:8080'
