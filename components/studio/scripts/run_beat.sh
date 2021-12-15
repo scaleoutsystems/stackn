77bdc2333e86d92ac1a6cfd4c184911d1db39162
carmat88 <techcarmat@pm.me>
2021-12-15 09:11:46 +0100

    Updating celery related tasks

--- components/studio/scripts/run_beat.sh
+++ components/studio/scripts/run_beat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sleep 20
+sleep 25
 
 watchmedo auto-restart -R --patterns="*.py" -- celery -A studio beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- components/studio/scripts/run_worker.sh
+++ components/studio/scripts/run_worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sleep 20
+sleep 25
 
 watchmedo auto-restart -R --patterns="*.py" -- celery -A studio worker -l info --scheduler django
