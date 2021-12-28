5daae34c1297421d4eba4258d89aa543173e0327
carmat88 <techcarmat@pm.me>
2021-12-28 16:44:49 +0100

    Updating root/landing url settings

--- components/studio/projects/urls.py
+++ components/studio/projects/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'projects'
 
 urlpatterns = [
-    path('', views.index, name='homepage'),
+    path('overview/', views.index, name='overview'),
     path('projects/', views.index, name='index'),
     path('projects/create', views.create, name='create'),
     path('projects/templates', views.project_templates, name='project_templates'),
