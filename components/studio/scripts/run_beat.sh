3b3e97e2e149189ec00123b3bb82c20b7d6937c2
TechCarmat <techcarmat@pm.me>
2022-01-28 13:00:37 +0100

    Adding comment to celery scripts

--- components/studio/scripts/run_beat.sh
+++ components/studio/scripts/run_beat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Giving time to studio container to run DB migrations
 sleep 25
 
 watchmedo auto-restart -R --patterns="*.py" -- celery -A studio beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- components/studio/scripts/run_worker.sh
+++ components/studio/scripts/run_worker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Giving time to studio container to run DB migrations
 sleep 25
 
 watchmedo auto-restart -R --patterns="*.py" -- celery -A studio worker -l info --scheduler django
