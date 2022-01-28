42360f3613085b18e3c0e4200cfe6c6217f17fa4
TechCarmat <techcarmat@pm.me>
2022-01-28 13:01:19 +0100

    Allowing to init DB only once with fixtures and data

--- components/studio/scripts/run_web.sh
+++ components/studio/scripts/run_web.sh
@@ -3,31 +3,36 @@
 # If we have set a local, custom settings.py, then use that.
 #[ -f studio/local_settings.py ] && echo "Using local settings file" && export DJANGO_SETTINGS_MODULE=studio.local_settings
 
-# echo "Running studio migrations..."
-python3 manage.py makemigrations
-python3 manage.py migrate
+INIT=$1 # should be either true or false
 
-# NOTE: The following fixtures and super user creation are executed as a helm post-install k8s job, thus disabled here.
-# However for testing and developement purpose, activate them when not using a post-install job
+# To allow setting up fixtures and init DB data for only the first time
+if $INIT; then
+    echo "Running studio migrations..."
+    python3 manage.py makemigrations
+    python3 manage.py migrate
 
-echo "Loading Studio Fixtures..."
-# Related to Projects (including project meta-resources such as flavours, environments, etc...)
-python3 manage.py loaddata projects/fixtures/projects_templates.json
-#Related to Apps (including celery tasks and intervals)
-python3 manage.py loaddata apps/fixtures/intervals_fixtures.json
-python3 manage.py loaddata apps/fixtures/periodic_tasks_fixtures.json
-python3 manage.py loaddata apps/fixtures/appcats_fixtures.json
-python3 manage.py loaddata apps/fixtures/apps_fixtures.json
-python3 manage.py runscript load_apps_logo -v2
-# Related to Models
-python3 manage.py loaddata models/fixtures/objecttype_fixtures.json
+    # NOTE: The following fixtures and super user creation are executed as a helm post-install k8s job, thus disabled here.
+    # However for testing and developement purpose, activate them when not using a post-install job
 
-# HELM deployment: DJANGO_SUPERUSER_PASSWORD should be an env var within the stackn-studio pod
-# python3 manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER --no-input
+    echo "Loading Studio Fixtures..."
+    # Related to Projects (including project meta-resources such as flavours, environments, etc...)
+    python3 manage.py loaddata projects/fixtures/projects_templates.json
+    #Related to Apps (including celery tasks and intervals)
+    python3 manage.py loaddata apps/fixtures/intervals_fixtures.json
+    python3 manage.py loaddata apps/fixtures/periodic_tasks_fixtures.json
+    python3 manage.py loaddata apps/fixtures/appcats_fixtures.json
+    python3 manage.py loaddata apps/fixtures/apps_fixtures.json
+    python3 manage.py runscript load_apps_logo -v2
+    # Related to Models
+    python3 manage.py loaddata models/fixtures/objecttype_fixtures.json
 
-# ONLY for local testing with docker-compose
-export DJANGO_SUPERUSER_PASSWORD='dGhpaXNhdmVyeW5vdHNhZmVvbmx'
-python3 manage.py createsuperuser --email 'admin@test.com' --username 'admin' --no-input
+    # HELM deployment: DJANGO_SUPERUSER_PASSWORD should be an env var within the stackn-studio pod
+    # python3 manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --username $DJANGO_SUPERUSER --no-input
+
+    # ONLY for local testing with docker-compose
+    export DJANGO_SUPERUSER_PASSWORD='dGhpaXNhdmVyeW5vdHNhZmVvbmx'
+    python3 manage.py createsuperuser --email 'admin@test.com' --username 'admin' --no-input
+fi
 
 echo "Starting the Studio server..."
 python3 manage.py runserver 0.0.0.0:8080
