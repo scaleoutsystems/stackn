a8c38d1a39daf486dc981d81cf81c155a44870fd
TechCarmat <techcarmat@pm.me>
2022-02-01 19:01:21 +0100

    Adding form field for creating new environments

--- components/studio/projects/views.py
+++ components/studio/projects/views.py
@@ -79,6 +79,9 @@ def settings(request, user, project_slug):
     s3instances = S3.objects.filter(project=project)
     flavors = Flavor.objects.filter(project=project)
     mlflows = MLFlow.objects.filter(project=project)
+    
+    registry_app = Apps.objects.get(slug='docker-registry')
+    registries = AppInstance.objects.filter(app=registry_app.pk, project=project)
 
     if request.method == 'POST':
         form = TransferProjectOwnershipForm(request.POST)
