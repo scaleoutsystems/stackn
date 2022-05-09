ae51ba9c9ae94588b180a029f8d3a149887ff995
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-05-09 16:21:01 +0200

    pre_save on user creation, option for inactive user

--- components/studio/studio/views.py
+++ components/studio/studio/views.py
@@ -2,6 +2,19 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from projects.models import Project
 
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.contrib.auth.models import User
+from django.conf import settings
+
+@receiver(pre_save, sender=User)
+def set_new_user_inactive(sender, instance, **kwargs):
+    if instance._state.adding is True and settings.INACTIVE_USERS:
+        print("Creating Inactive User")
+        instance.is_active = False
+    else:
+        print("Updating User Record")
+
 # Since this is a production feature, it will only work if DEBUG is set to False
 def handle_page_not_found(request, exception):
     return HttpResponseRedirect('/')
