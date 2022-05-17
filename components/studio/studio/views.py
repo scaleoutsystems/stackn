7656de57d108ec8978e19bbb85d15af24cc4eea7
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-05-17 16:23:42 +0200

    Feature/SK-145 Serve Apps and ingress auth (#280)

--- components/studio/studio/views.py
+++ components/studio/studio/views.py
@@ -1,11 +1,17 @@
 from django.http import HttpResponseRedirect
-from django.http import HttpResponse
 from projects.models import Project
+from apps.models import AppInstance
 
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
 from django.contrib.auth.models import User
 from django.conf import settings
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 @receiver(pre_save, sender=User)
 def set_new_user_inactive(sender, instance, **kwargs):
@@ -19,12 +25,34 @@ def set_new_user_inactive(sender, instance, **kwargs):
 def handle_page_not_found(request, exception):
     return HttpResponseRedirect('/')
 
-def auth(request):
-    if request.user.is_authenticated:
-        project_slug = request.GET.get('project')
-        project = Project.objects.get(slug=project_slug)
-        if request.user.has_perm('can_view_project', project):
-            return HttpResponse(status=200)
+class AccessPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        """
+        Should simply return, or raise a 403 response.
+        """
+        try:
+            release = request.GET.get('release')
+            app_instance = AppInstance.objects.get(parameters__contains={'release':release})
+            project = app_instance.project
+        except:
+            project_slug = request.GET.get('project')
+            project = Project.objects.get(slug=project_slug)
+            return request.user.has_perm('can_view_project', project)
+        
+        if app_instance.access == "private":
+            return app_instance.owner == request.user
+        elif app_instance.access == "project":
+            return request.user.has_perm('can_view_project', project)
         else:
-            return HttpResponse(status=403)
-    return HttpResponse(status=401)
+            return True
+class AuthView(APIView):
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated, AccessPermission]
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),
+            'auth': str(request.auth),
+        }
+        return Response(content)
