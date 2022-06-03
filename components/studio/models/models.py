1d5e0818c047625f4ea8fd79cac3401fb19f7182
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-06-03 15:15:13 +0200

    Feature/SK-164 Update gh workflows, imports and formatting checks (#289)

--- components/studio/models/models.py
+++ components/studio/models/models.py
@@ -1,15 +1,17 @@
-from django.conf import settings
-from django.db import models
-from django.dispatch import receiver
-from django.db.models.signals import pre_delete, pre_save
-from django import forms
-from django.utils.module_loading import import_string
 # from deployments.models import DeploymentInstance
 from ast import literal_eval
 from functools import cmp_to_key
-from projects.helpers import get_minio_keys
+
+from django import forms
+from django.conf import settings
+from django.db import models
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+from django.utils.module_loading import import_string
 from minio import Minio
 from tagulous.models import TagField
+
+from projects.helpers import get_minio_keys
 
 
 def compare_version(v1, v2):
@@ -22,6 +24,7 @@ def compare_version(v1, v2):
         return 1
     else:
         return 0
+
 
 class ModelManager(models.Manager):
 
@@ -45,9 +48,12 @@ class ModelManager(models.Manager):
 
         return []
 
+
 class ObjectType(models.Model):
-    name = models.CharField(max_length=100, null=True, blank=True, default="Model")
-    slug = models.CharField(max_length=100, null=True, blank=True, default="model")
+    name = models.CharField(max_length=100, null=True,
+                            blank=True, default="Model")
+    slug = models.CharField(max_length=100, null=True,
+                            blank=True, default="model")
     app_slug = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
@@ -89,16 +95,20 @@ class Model(models.Model):
     uid = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     version = models.CharField(max_length=255, default='1.0')
-    release_type = models.CharField(max_length=255, choices=RELEASE_TYPE, default=MINOR)
+    release_type = models.CharField(
+        max_length=255, choices=RELEASE_TYPE, default=MINOR)
     description = models.CharField(max_length=255, null=True, blank=True)
     model_card = models.TextField(null=True, blank=True)
     access = models.CharField(max_length=2, choices=ACCESS, default=PRIVATE)
     resource = models.URLField(max_length=2048, null=True, blank=True)
     object_type = models.ManyToManyField(ObjectType, blank=True)
     url = models.URLField(max_length=512, null=True, blank=True)
-    s3 = models.ForeignKey('projects.S3', null=True, blank=True, on_delete=models.CASCADE)
-    bucket = models.CharField(max_length=200, null=True, blank=True, default="models")
-    path = models.CharField(max_length=200, null=True, blank=True, default="models")
+    s3 = models.ForeignKey('projects.S3', null=True,
+                           blank=True, on_delete=models.CASCADE)
+    bucket = models.CharField(
+        max_length=200, null=True, blank=True, default="models")
+    path = models.CharField(max_length=200, null=True,
+                            blank=True, default="models")
     uploaded_at = models.DateTimeField(auto_now_add=True)
     tags = TagField(blank=True)
     project = models.ForeignKey(
@@ -108,15 +118,17 @@ class Model(models.Model):
         null=True)
     status = models.CharField(max_length=2, choices=STATUS, default=CREATED)
     # tag = models.CharField(max_length=10, default='latest')
-    model_card_headline = models.ImageField(upload_to='models/image', null=True, blank=True, default=None)
+    model_card_headline = models.ImageField(
+        upload_to='models/image', null=True, blank=True, default=None)
     docker_image = models.OneToOneField('projects.Environment', null=True, blank=True,
                                         on_delete=models.CASCADE, default=None)
 
     class Meta:
         unique_together = ('name', 'version', 'project')
 
-    def __str__(self): 
+    def __str__(self):
         return "{name}:{version}".format(name=self.name, version=self.version)
+
 
 class ModelLog(models.Model):
     STARTED = 'ST'
@@ -129,10 +141,10 @@ class ModelLog(models.Model):
     ]
     run_id = models.CharField(max_length=32)
     trained_model = models.CharField(max_length=32, default='')
-    #trained_model = models.ForeignKey(
-    #    Model, 
+    # trained_model = models.ForeignKey(
+    #    Model,
     #    on_delete=models.CASCADE
-    #)
+    # )
     project = models.CharField(max_length=255, default='')
     training_started_at = models.CharField(max_length=255)
     #training_started_at = models.DateTimeField(auto_now_add=True)
@@ -142,10 +154,12 @@ class ModelLog(models.Model):
     latest_git_commit = models.CharField(max_length=255, default='')
     system_details = models.TextField(blank=True)
     cpu_details = models.TextField(blank=True)
-    training_status = models.CharField(max_length=2, choices=STATUS, default=STARTED)
+    training_status = models.CharField(
+        max_length=2, choices=STATUS, default=STARTED)
 
     class Meta:
         unique_together = ('run_id', 'trained_model')
+
 
 class Metadata(models.Model):
     run_id = models.CharField(max_length=32)
@@ -174,12 +188,13 @@ def pre_save_model(sender, instance, using, **kwargs):
             new_version = VERSION_CLASS()
         else:
             new_version = VERSION_CLASS(model.version)
-        
 
         release_status, instance.version = new_version.release(release_type)
         print('New version: '+instance.version)
         if not release_status:
-            raise Exception('Failed to create new release for model {}-{}, release type {}.'.format(instance.name, instance.version, release_type))
+            raise Exception('Failed to create new release for model {}-{}, release type {}.'.format(
+                instance.name, instance.version, release_type))
+
 
 @receiver(pre_delete, sender=Model, dispatch_uid='model_pre_delete_signal')
 def pre_delete_model(sender, instance, using, **kwargs):
@@ -188,9 +203,9 @@ def pre_delete_model(sender, instance, using, **kwargs):
     minio_keys = get_minio_keys(instance.project)
     try:
         client = Minio(instance.project.s3storage.host,
-                      access_key=instance.project.s3storage.access_key,
-                      secret_key=instance.project.s3storage.secret_key,
-                      secure=settings.OIDC_VERIFY_SSL)
+                       access_key=instance.project.s3storage.access_key,
+                       secret_key=instance.project.s3storage.secret_key,
+                       secure=settings.OIDC_VERIFY_SSL)
         client.remove_object('models', instance.uid)
     except:
         print('Failed to delete model object {} from minio store.'.format(instance.uid))
--- components/studio/models/templatetags/custom_tags.py
+++ components/studio/models/templatetags/custom_tags.py
@@ -1,22 +1,27 @@
-from django import template
-from django.db.models.functions import Length
 import json
 
+from django import template
+from django.db.models.functions import Length
+
 register = template.Library()
+
 
 @register.filter(name='split')
 def split(value, arg):
     return value.all().order_by(Length('name').desc())
 
+
 @register.filter
 def sort_by(queryset, order):
     return queryset.order_by(order)
+
 
 @register.filter(name='exists')
 def exists(value, arg):
     if str(arg) in value:
         return True
     return False
+
 
 @register.filter(name='count_str')
 def count_str(value):
@@ -31,8 +36,9 @@ def count_str(value):
         num_tags += 1
     if num_tags == 0:
         num_tags = 1
-    print("LIMITS: ", num_tags,total_length)
+    print("LIMITS: ", num_tags, total_length)
     return num_tags
+
 
 @register.filter(name='subtract')
 def subtract(value, arg):
