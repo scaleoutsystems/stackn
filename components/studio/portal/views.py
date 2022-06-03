43223eb436f325dcb648fe56cb78c87882994150
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-06-03 15:15:13 +0200

    Feature/SK-164 Update gh workflows, imports and formatting checks (#289)

--- components/studio/portal/views.py
+++ components/studio/portal/views.py
@@ -1,31 +1,32 @@
-import requests
-import uuid
 import time
+import uuid
 
-from django.shortcuts import render, HttpResponseRedirect, reverse, redirect
-from django.http import JsonResponse
+import requests
 from django.conf import settings
-from django.utils.text import slugify
-from django.db.models import Q
-from django.template import engines
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+from django.db.models import Q
+from django.http import JsonResponse
+from django.shortcuts import HttpResponseRedirect, redirect, render, reverse
+from django.template import engines
+from django.utils.text import slugify
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
-
-from apps.models import Apps, AppInstance
+from apps.models import AppInstance, Apps
 from projects.models import Project
 
-def index(request,id=0):
+
+def index(request, id=0):
     try:
-        projects = Project.objects.filter(Q(owner=request.user) | Q(authorized=request.user), status='active')
+        projects = Project.objects.filter(Q(owner=request.user) | Q(
+            authorized=request.user), status='active')
     except Exception as err:
         print("User not logged in.")
     #base_template = 'base.html'
     if 'project' in request.session:
         project_slug = request.session['project']
 
-        #if request.user.is_authenticated:
+        # if request.user.is_authenticated:
         #    try:
         #        project = Project.objects.filter(Q(owner=request.user) | Q(authorized=request.user), status='active', slug=project_slug).first()
         #        base_template = 'baseproject.html'
@@ -39,7 +40,7 @@ def index(request,id=0):
     #     print(project)
     #     print(base_template)
     media_url = settings.MEDIA_URL
-    
+
     # create session object to store info about app and their tag counts
     if "app_tags" not in request.session:
         request.session['app_tags'] = {}
@@ -48,21 +49,22 @@ def index(request,id=0):
         # add app id to app_tags object
         if "app_id_add" in request.GET:
             num_tags = int(request.GET['tag_count'])
-            id=int(request.GET['app_id_add'])
-            request.session['app_tags'][str(id)]=num_tags
+            id = int(request.GET['app_id_add'])
+            request.session['app_tags'][str(id)] = num_tags
         # remove app id from app_tags object
         if "app_id_remove" in request.GET:
             num_tags = int(request.GET['tag_count'])
-            id=int(request.GET['app_id_remove'])
+            id = int(request.GET['app_id_remove'])
             if str(id) in request.session['app_tags']:
                 request.session['app_tags'].pop(str(id))
-    
+
     # reset app_tags if Apps Tab on Sidebar pressed
-    if id==0:
+    if id == 0:
         if 'tf_add' not in request.GET and 'tf_remove' not in request.GET:
             request.session['app_tags'] = {}
-    
-    published_apps = AppInstance.objects.filter(~Q(state='Deleted'), access='public')
+
+    published_apps = AppInstance.objects.filter(
+        ~Q(state='Deleted'), access='public')
 
     # create session object to store ids for tag seacrh if it does not exist
     if "app_tag_filters" not in request.session:
@@ -75,8 +77,8 @@ def index(request,id=0):
         tag = request.GET['tf_remove']
         if tag in request.session['app_tag_filters']:
             request.session['app_tag_filters'].remove(tag)
-    elif "tag_count"  not in request.GET:
-        tag=""
+    elif "tag_count" not in request.GET:
+        tag = ""
         request.session['app_tag_filters'] = []
     print("app_tag_filters: ", request.session['app_tag_filters'])
 
@@ -89,7 +91,7 @@ def index(request,id=0):
                     tagged_published_apps.append(app)
                     break
         published_apps = tagged_published_apps
-        
+
     request.session.modified = True
 
     template = 'portal/index.html'
@@ -101,5 +103,4 @@ class HomeView(View):
 
     def get(self, request):
 
-        
         return render(request, self.template, locals())
