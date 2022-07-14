069fdce743b147fab64ef292ae73d7eeb63fd6ea
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-07-14 14:54:49 +0200

    Release/v0.6.0 (#299)

--- components/studio/apps/models.py
+++ components/studio/apps/models.py
@@ -1,56 +1,54 @@
-from django.db import models
-from django.utils.text import slugify
-from django.dispatch import receiver
-from django.db.models.signals import pre_delete, pre_save
-from django.conf import settings
-from django.template import engines
-from models.models import Model
-from projects.models import Project, ReleaseName
-from projects.helpers import get_minio_keys
 from django.contrib.auth.models import User
-from modules import keycloak_lib as keylib
-import uuid
-import flatten_json
-from datetime import datetime, timedelta
+from django.db import models
 from tagulous.models import TagField
+
+from models.models import Model
+from projects.models import Project
 
 
 class AppPermission(models.Model):
+    appinstance = models.OneToOneField(
+        'apps.AppInstance', on_delete=models.CASCADE, null=True, related_name="permission")
     name = models.CharField(max_length=512, default="permission_name")
-    public = models.BooleanField(default=False)
     projects = models.ManyToManyField('projects.Project')
+    public = models.BooleanField(default=False)
     users = models.ManyToManyField(User)
-    appinstance = models.OneToOneField('apps.AppInstance', on_delete=models.CASCADE, null=True, related_name="permission")
+
     def __str__(self):
         return str(self.name)
+
 
 class AppCategories(models.Model):
     name = models.CharField(max_length=512)
-    slug = models.CharField(max_length=512, default="", primary_key=True)
     priority = models.IntegerField(default=100)
+    slug = models.CharField(max_length=512, default="", primary_key=True)
+
     def __str__(self):
         return str(self.name)
 
+
 class Apps(models.Model):
-    name = models.CharField(max_length=512)
-    slug = models.CharField(max_length=512, blank=True, null=True)
-    category = models.ForeignKey('AppCategories', related_name="apps", on_delete=models.CASCADE, null=True)
-    settings = models.JSONField(blank=True, null=True)
+    user_can_create = models.BooleanField(default=True)
+    access = models.CharField(
+        max_length=20, blank=True, null=True, default="public")
+    category = models.ForeignKey(
+        'AppCategories', related_name="apps", on_delete=models.CASCADE, null=True)
     chart = models.CharField(max_length=512)
-
     chart_archive = models.FileField(upload_to='apps/', null=True, blank=True)
-    revision = models.IntegerField(default=1)
-
-    description = models.TextField(blank=True, null=True, default="")
-    table_field = models.JSONField(blank=True, null=True)
-    logo = models.CharField(max_length=512, default="dist/applogos/stackn_logo_square.png")
-    logo_file = models.FileField(upload_to='apps/logos/', null=True, blank=True)
-    priority = models.IntegerField(default=100)
-    access = models.CharField(max_length=20, blank=True, null=True, default="public")
-    projects = models.ManyToManyField('projects.Project')
-    updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    
+    description = models.TextField(blank=True, null=True, default="")
+    logo = models.CharField(max_length=512, default="")
+    logo_file = models.ImageField(
+        upload_to='apps/logos', null=True, blank=True)
+    name = models.CharField(max_length=512)
+    priority = models.IntegerField(default=100)
+    projects = models.ManyToManyField('projects.Project')
+    revision = models.IntegerField(default=1)
+    settings = models.JSONField(blank=True, null=True)
+    slug = models.CharField(max_length=512, blank=True, null=True)
+    table_field = models.JSONField(blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         unique_together = ('slug', 'revision',)
 
@@ -59,45 +57,48 @@ class Apps(models.Model):
 
 
 class AppInstance(models.Model):
-    # TODO: Name, project should be unique combination
-    name = models.CharField(max_length=512, default="app_name")
-    app = models.ForeignKey('Apps', on_delete=models.CASCADE, related_name='appinstance')
-    project = models.ForeignKey('projects.Project', on_delete=models.CASCADE, related_name='appinstance')
+    access = models.CharField(
+        max_length=20, default="private", null=True, blank=True)
+    app = models.ForeignKey(
+        'Apps', on_delete=models.CASCADE, related_name='appinstance')
     app_dependencies = models.ManyToManyField('apps.AppInstance', blank=True)
-    model_dependencies = models.ManyToManyField('models.Model', blank=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='app_owner', null=True)
-    # url = models.CharField(max_length=512, null=True)
-    info = models.JSONField(blank=True, null=True)
-    parameters = models.JSONField(blank=True, null=True)
-    state = models.CharField(max_length=50, null=True, blank=True)
-    table_field = models.JSONField(blank=True, null=True)
-
-    access = models.CharField(max_length=20, default="private", null=True, blank=True)
-    tags = TagField()
-    updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     deleted_on = models.DateTimeField(null=True, blank=True)
+    info = models.JSONField(blank=True, null=True)
+    model_dependencies = models.ManyToManyField('models.Model', blank=True)
+    name = models.CharField(max_length=512, default="app_name")
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='app_owner', null=True)
+    parameters = models.JSONField(blank=True, null=True)
+    project = models.ForeignKey(
+        'projects.Project', on_delete=models.CASCADE, related_name='appinstance')
+    state = models.CharField(max_length=50, null=True, blank=True)
+    table_field = models.JSONField(blank=True, null=True)
+    tags = TagField()
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)+' ({})-{}-{}-{}'.format(self.state, self.owner, self.app.name, self.project)
 
+
 class AppStatus(models.Model):
-    appinstance = models.ForeignKey('AppInstance', on_delete=models.CASCADE, related_name="status")
+    appinstance = models.ForeignKey(
+        'AppInstance', on_delete=models.CASCADE, related_name="status")
+    info = models.JSONField(blank=True, null=True)
     status_type = models.CharField(max_length=15, default="app_name")
     time = models.DateTimeField(auto_now_add=True)
-    info = models.JSONField(blank=True, null=True)
 
     class Meta:
         get_latest_by = 'time'
 
     def __str__(self):
         return str(self.appinstance.name)+"({})".format(self.time)
-    
 
 
 class ResourceData(models.Model):
-    appinstance = models.ForeignKey('AppInstance', on_delete=models.CASCADE, related_name="resourcedata")
+    appinstance = models.ForeignKey(
+        'AppInstance', on_delete=models.CASCADE, related_name="resourcedata")
     cpu = models.IntegerField()
-    mem = models.IntegerField()
     gpu = models.IntegerField()
+    mem = models.IntegerField()
     time = models.IntegerField()
--- components/studio/apps/urls.py
+++ components/studio/apps/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from . import views
 
 app_name = 'apps'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('apps/', views.index, name='index'),
     path('status', views.get_status, name='get_status'),
     path('<category>', views.filtered, name='filtered'),
     path('create/<app_slug>', views.create, name='create'),
