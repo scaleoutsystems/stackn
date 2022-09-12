a91fdeb05cd73df88c8c9c2752326df6b354ec3a
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-09-12 12:53:25 +0200

    make python package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='studio-portal',
+    version='0.0.1',
+    description="""Django app for handling portal in Studio""",
+    url='https://www.scaleoutsystems.com',
+    include_package_data=True,
+    py_modules=['studio_portal'],
+    python_requires='>=3.6,<4',
+    install_requires=[
+        "django==4.0.6",
+        "requests==2.27.1",
+        "Pillow==9.0.0"
+    ],
+    license="Copyright Scaleout Systems AB. See license for details",
+    zip_safe=False,
+    keywords='',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
