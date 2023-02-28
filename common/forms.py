7a50f74c63b2a903f2482035032c230aa3ee649c
Niklas <niklas@scaleoutsystems.com>
2023-02-28 12:29:53 +0100

    Sign up form | added placeholders and css classes to inputs

--- common/forms.py
+++ common/forms.py
@@ -5,13 +5,49 @@ from django.contrib.auth.models import User
 
 # Sign Up Form
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=30,
+        required=True,
+        label=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Username*"}
+        ),
+    )
     first_name = forms.CharField(
-        max_length=30, required=False, help_text='Optional')
+        max_length=30,
+        required=False,
+        label=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "First name"}
+        ),
+    )
     last_name = forms.CharField(
-        max_length=30, required=False, help_text='Optional')
+        max_length=30,
+        required=False,
+        label=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Last name"}
+        ),
+    )
     email = forms.EmailField(
-        max_length=254, help_text='Enter a valid email address')
-
+        max_length=254,
+        label=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Email*"}
+        ),
+    )
+    password1 = forms.CharField(
+        label=False,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password*"}
+        ),
+    )
+    password2 = forms.CharField(
+        label=False,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Confirm"}
+        ),
+    )
     class Meta:
         model = User
         fields = [
