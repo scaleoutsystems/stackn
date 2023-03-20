550de9abf3d4223a9448bc5c9710ef5e9b375e6b
Niklas <niklas@scaleoutsystems.com>
2023-03-20 14:48:15 +0100

    Added template tag required for app per project limit

---
+++ customtags/templatetags/can_create_app.py
@@ -0,0 +1,17 @@
+from django import template
+
+from apps.models import AppInstance
+
+register = template.Library()
+
+
+# settings value
+@register.simple_tag
+def can_create_app(user, project, app):
+    app_slug = app.slug
+
+    user_can_create = AppInstance.objects.user_can_create(
+        user=user, project=project, app_slug=app_slug
+    )
+
+    return user_can_create
