46a16ce1fae67da66a8f684fe761496b1aecbfbc
Fredrik Wrede <fredrik@scaleoutsystems.com>
2023-03-21 14:30:34 +0100

    Squashed 'models/' changes from 50d21173..39193eee
    39193eee Bug/SK-382 | Default version for model is always v1 when user selects serve from models list.  (#95)
    c7c3322a Bug/SK-381 | It is possible to create an empty tag (#91)
    a7e60cf1 Feature/SK-363 | Remove old CSS and Update bootstrap version (#85)
    
    git-subtree-dir: models
    git-subtree-split: 39193eeeb9432a8f7354367675e5fa9869e23d2d

--- admin.py
+++ admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 
 from .models import Metadata, Model, ModelLog, ObjectType
 
-admin.site.register(Model)
+
+class ModelAdmin(admin.ModelAdmin):
+    empty_value_display = "-"
+    list_display = ("name", "version", "project_name", "object_type_name")
+
+    def project_name(self, obj):
+        return obj.project.name
+
+    def object_type_name(self, obj):
+        return obj.object_type.name
+
+
+admin.site.register(Model, ModelAdmin)
 admin.site.register(ObjectType)
 admin.site.register(ModelLog)
 admin.site.register(Metadata)
