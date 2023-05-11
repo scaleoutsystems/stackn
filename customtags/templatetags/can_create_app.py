e299524c5578aa7c859eb7d156be824e84b66f08
Niklas <niklas@scaleoutsystems.com>
2023-05-11 17:11:06 +0200

    Side/Nav-bar fix + Template tag update

--- customtags/templatetags/can_create_app.py
+++ customtags/templatetags/can_create_app.py
@@ -8,7 +8,7 @@ register = template.Library()
 # settings value
 @register.simple_tag
 def can_create_app(user, project, app):
-    app_slug = app.slug
+    app_slug = app if isinstance(app, str) else app.slug
 
     user_can_create = AppInstance.objects.user_can_create(
         user=user, project=project, app_slug=app_slug
