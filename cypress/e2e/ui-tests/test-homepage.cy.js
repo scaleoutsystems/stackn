3a587e615baa3a740079bbd8fbfe356684a0ab26
akochari <arnold.kochari@scilifelab.se>
2023-09-12 14:49:25 +0200

    modify the test to work with empty wiki

--- cypress/e2e/ui-tests/test-homepage.cy.js
+++ cypress/e2e/ui-tests/test-homepage.cy.js
@@ -24,7 +24,7 @@ describe("Test of the home page", () => {
         cy.get("li.nav-item a").contains("User guide").click()
         cy.url().should("include", "/docs/")
         cy.get('[data-cy="sidebar-title"]').should('contain', 'user guide') // check that the sidebar title is there, comes from our templates
-        cy.get('#article-menu > .nav').contains("View") // check that the page menu is there, comes from django-wiki templates
+        cy.get('.col-lg-8 > h1').should('contain', 'Congratulations') // check that the page content is there, comes from django-wiki templates
     })
 
     it("should open the signup page on link click", () => {
